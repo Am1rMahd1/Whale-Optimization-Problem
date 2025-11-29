2d407048887139b04c0343a64015b56d3b203bf1
Amir Mahdi Sedghi <asedghi1380@gmail.com>
2025-11-29 11:43:31 +0330

    feat: add code to choose movement strategy

---
+++ src/Strategies/SearchForPrey.py
@@ -0,0 +1,2 @@
+class SearchForPrey:
+    pass
--- src/whale_optimization_algorithm/main.py
+++ src/whale_optimization_algorithm/main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import plotly.express as px
 
+from src.Strategies.BubbleNetAttackingMethod import BubbleNetAttackingMethod
+from src.Strategies.EncirclingPrey import EncirclingPrey
+from src.Strategies.SearchForPrey import SearchForPrey
+
 
 def create_data(distribution, how_many, dimensional):
     if distribution == 'uniform':
@@ -23,19 +27,51 @@ def findTheXStarIndex(fitnessVector):
     return np.argmin(fitnessVector)
 
 
+def chooseMovementStrategyRandomly(probability, A_parameter):
+    if probability >= 0.5:
+        if np.abs(A_parameter) < 1:
+            return EncirclingPrey()
+        else:
+            return SearchForPrey()
+    else:
+        return BubbleNetAttackingMethod()
+
+
+def throwACoinBetween0to1():
+    probability = np.random.random_sample()
+    return round(probability, 2)
+
+
+def a(iteration_number, max_iteration):
+    # -2/max * t + 2
+    return 2 - iteration_number * 2 / max_iteration
+
+
+def A(a_parameter):
+    r = throwACoinBetween0to1()
+    return 2 * a_parameter * r - a_parameter
+
+
 def whaleOptimizationAlgorithm(data, max_iteration):
     # make a copy of original data
     data = data.copy()
-    for iteration in range(max_iteration):
+    for iteration_number in range(max_iteration):
         fitnessVector = calculateFitnessForAllData(data)
 
         x_star_index = findTheXStarIndex(fitnessVector)
 
         x_star_value = data[x_star_index]
 
-        print(x_star_value)
+        probability = throwACoinBetween0to1()
 
-        # choose the movement type
+
+        # a parameter is a number that is reducing from 2 to 0 during iterations
+        a_parameter = a(iteration_number, max_iteration)
+
+        A_parameter = A(a_parameter)
+
+        movementObject = chooseMovementStrategyRandomly(probability, A_parameter)
+
 
         # calculate the distance
 
