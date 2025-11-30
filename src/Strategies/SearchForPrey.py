d1e90345df903e63279f0b2c08d382ede87dabf7
Amir Mahdi Sedghi <asedghi1380@gmail.com>
2025-11-30 21:03:00 +0330

    feat: add SearchForPrey strategy
    Signed-off-by: Amir Mahdi Sedghi <asedghi1380@gmail.com>

--- src/Strategies/SearchForPrey.py
+++ src/Strategies/SearchForPrey.py
@@ -1,2 +1,6 @@
-class SearchForPrey:
-    pass
+from src.Strategies.EncirclingPrey import EncirclingPrey
+
+
+class SearchForPrey(EncirclingPrey):
+    def __init__(self, A_parameter):
+        super().__init__(A_parameter)
